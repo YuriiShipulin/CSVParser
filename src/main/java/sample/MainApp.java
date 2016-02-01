4ddddfe4e51e2b03a2bb346bcf4b19c4768a2bab
yuriishipulin <yurii.shipulin.31@gmail.com>
2016-02-01 16:36:20 +0200

    commit #4

--- src/main/java/sample/MainApp.java
+++ src/main/java/sample/MainApp.java
@@ -6,11 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
         primaryStage.setTitle("CSV_Parser");
         primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
